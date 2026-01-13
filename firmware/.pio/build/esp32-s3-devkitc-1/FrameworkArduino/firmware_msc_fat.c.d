1a354d66e4cff6ea1f1a98d1a8ee1a95ff925067
miguelpicado <miguelpicadofilgueira@gmail.com>
2026-01-13 13:18:12 +0100

    Fix History TripCard prop in FullScreenBYD and remove firmware folder

--- firmware/.pio/build/esp32-s3-devkitc-1/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/FrameworkArduino/firmware_msc_fat.c.o: \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32@src-e5a7fd5d9117bd8a322277fde59ac3d3/cores/esp32/firmware_msc_fat.c \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32@src-e5a7fd5d9117bd8a322277fde59ac3d3/cores/esp32/firmware_msc_fat.h
--- firmware/.pio/build/esp32-s3-devkitc-1/FrameworkArduino/stdlib_noniso.c.d
+++
@@ -1,9 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/FrameworkArduino/stdlib_noniso.c.o: \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32@src-e5a7fd5d9117bd8a322277fde59ac3d3/cores/esp32/stdlib_noniso.c \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32@src-e5a7fd5d9117bd8a322277fde59ac3d3/cores/esp32/stdlib_noniso.h \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/esp_system/include/esp_system.h \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/esp_common/include/esp_err.h \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/esp_common/include/esp_compiler.h \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/esp_common/include/esp_attr.h \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/esp_common/include/esp_bit_defs.h \
- C:/Users/migue/.platformio/packages/framework-arduinoespressif32-libs/esp32s3/include/esp_common/include/esp_idf_version.h
