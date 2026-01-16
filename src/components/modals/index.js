8c75835f3b909927583fa4a069f1dcf3904689ee
miguelpicado <miguelpicadofilgueira@gmail.com>
2026-01-16 15:39:48 +0100

    chore: cleanup unused files and sync translations, fix build

--- src/components/modals/index.js
+++
@@ -1,7 +0,0 @@
-// BYD Stats - Modals barrel export
-
-export { default as SettingsModal } from './SettingsModal';
-export { default as FilterModal } from './FilterModal';
-export { default as TripDetailModal } from './TripDetailModal';
-export { default as HistoryModal } from './HistoryModal';
-export { default as DatabaseUploadModal } from './DatabaseUploadModal';
