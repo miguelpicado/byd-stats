121de514dd0a40756e42a07a3053eed27d2c3cc2
miguelpicado <miguelpicadofilgueira@gmail.com>
2026-01-29 12:19:08 +0100

    fix(hooks): add missing useMemo import in useGoogleSync

--- src/hooks/useGoogleSync.js
+++ src/hooks/useGoogleSync.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useMemo } from 'react';
 import { useGoogleLogin } from '@react-oauth/google';
 import { googleDriveService } from '../services/googleDrive';
 import { Capacitor } from '@capacitor/core';
